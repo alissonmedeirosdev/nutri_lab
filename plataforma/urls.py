4ccd80636ee1df3066540646a2aa9e687c606cf7
alissonmedeirosdev <alissonmelo.work@gmail.com>
2025-02-20 10:35:57 -0300

    adds patient platform front-end

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('pacientes/', views.paciente, name='pacientes')
+    path('pacientes/', views.pacientes, name='pacientes')
 ]
